7b59232ec85cc9da17040c087b24a7b476e54f9a
selestrel07 <selestrel07@gmail.com>
2023-11-09 16:59:42 +0400

    update signin api and comment password encryption

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email 
         && req.body.password === database.users[0].password) {
-            res.json('success');
+            res.json(database.users[0]);
         } else {
             res.status(400).json('error logging in');
         }
@@ -45,9 +45,9 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
     
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     console.log(hash);
+    // });
     database.users.push({
         id: '125',
         name: name,
