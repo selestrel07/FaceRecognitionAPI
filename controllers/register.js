b918e904b1e71f1c47026c27d33a527b4051eb22
selestrel07 <selestrel07@gmail.com>
2023-11-10 18:01:18 +0400

    add validations to register and signin controllers

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,8 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
     const { email, name, password } = req.body;
-    
+    if (!email || !name || !password) {
+        return res.status.json('incorrect form submission');
+    }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
         trx.insert({
